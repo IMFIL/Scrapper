96894fad26ca0afba559d37a66c36d90397042a1
Filip Slatinac <fslat092@uottawa.ca>
2016-10-05 18:11:45 -0400

    commiting for the first time in a while

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 from __future__ import print_function                 
 from flask import Flask, render_template, request, url_for,Response,redirect
 import urllib2
-import sqlite3 as lite
 import json
 import jinja2
+from bs4 import BeautifulSoup
 import math
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
-	return render_template('index.html')
+	link = request.args.get('websiteLink')
+	if link == None:
+		return render_template('index.html')
+
+	else:
+		try:
+			page = urllib2.urlopen(link).read()
+		except ValueError:
+			return "for now, you only get a string, but will make a error page :)"
+
+		soup = BeautifulSoup(page,"html.parser")
+		soup.prettify()
+		print(soup)
+		print(type(soup))
+		return "done"
+
+		# return a template that is exactly like index.html but has the results displayed in the search section
 
 
 if __name__ =="__main__":
